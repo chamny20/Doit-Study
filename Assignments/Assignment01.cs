647f36ddf084a8eb2c8bf89b5f06b22e6e5115be
chamny20 <dge3179@ajou.ac.kr>
2021-04-04 23:14:21 +0900

    week3 init

--- Assignments/Assignment01.cs
+++ Assignments/Assignment01.cs
@@ -12,7 +12,11 @@ namespace DoitStudy.Assignments
             string inputData = ((TestCase01.TestCase)data).inputData; // inputData
             //res에 값을 저장시켜주세요.
             Toy[] res = {}; // 파싱하고 값을 바르게 넣기
-            
+            foreach(var toy in inputData){
+                    res += $"{toy.name} {toy.year} {toy.price}\r\n";
+                    if(toy.point != (toy.year % 100) * (toy.price / 1000))
+                        return false;
+                }            
 
             //return 값은 Toy Class의 Array 형식으로 Toy[] 입니다.
             return res;
